e73681edee6dcdcb9d3625cdfe777468b7d4cdc8
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2023-09-25 13:50:57 +0600

    age taking only numerical number

--- form.js
+++ form.js
@@ -8,9 +8,19 @@ btn.addEventListener('click', function(){
 function getInput(name, age, gender) {
     var userName = document.getElementById(name).value;
     var userAge = document.getElementById(age).value;
+    if(isNaN(userAge)){
+        alert("give a number as input");
+    }
+    else{
+        document.getElementById("ageFill").innerText = userAge;
+    }
     var userGender = document.getElementById(gender).value;
 
     document.getElementById("nameFill").innerText = userName;
-    document.getElementById("ageFill").innerText = userAge;
+    // document.getElementById("ageFill").innerText = userAge;
     document.getElementById("genderFill").innerText = userGender;
+
+    var userName = document.getElementById(name).value = "";
+    var userAge = document.getElementById(age).value = "";
+    var userGender = document.getElementById(gender).value = "";
 }
