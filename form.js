9112f0ef2a1c8eb7a975596290c4572494e6e5bd
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2023-09-25 17:23:08 +0600

    delete button added

--- form.js
+++ form.js
@@ -1,26 +1,38 @@
 let btn = document.getElementById('submit');
 
 btn.addEventListener('click', function(){
-    console.log('btn hit'); //debugger
+    console.log('submit button hit'); //debugger
     getInput("name", "age", "gender");
 })
 
 function getInput(name, age, gender) {
-    var userName = document.getElementById(name).value;
-    var userAge = document.getElementById(age).value;
-    if(isNaN(userAge)){
-        alert("Please enter valid age");
-    }
-    else{
+    let userName = document.getElementById(name).value;
+    let userAge = document.getElementById(age).value;
+    if (isNaN(userAge)) {
+        alert("Please enter a valid age");
+    } else {
         document.getElementById("ageFill").innerText = userAge;
     }
-    var userGender = document.getElementById(gender).value;
+    let userGender = document.getElementById(gender).value;
 
     document.getElementById("nameFill").innerText = userName;
-    // document.getElementById("ageFill").innerText = userAge;
     document.getElementById("genderFill").innerText = userGender;
 
-    var userName = document.getElementById(name).value = "";
-    var userAge = document.getElementById(age).value = "";
-    var userGender = document.getElementById(gender).value = "";
+    document.getElementById(name).value = "";
+    document.getElementById(age).value = "";
+    document.getElementById(gender).value = "";
+}
+
+
+let deleteBtn = document.getElementById('delete');
+
+deleteBtn.addEventListener('click', function(){
+    console.log('delete button hit');
+    deleteInput("nameFill", "ageFill", "genderFill")
+})
+
+function deleteInput(name, age, gender) {
+    document.getElementById(name).innerText = "";
+    document.getElementById(age).innerText = "";
+    document.getElementById(gender).innerText = "";
 }
